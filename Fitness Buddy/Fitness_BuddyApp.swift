c1535876e1e6d503ed0b9cedd5abe4db8de7ed58
Alex Le <Alexle8426@gmail.com>
2026-05-26 21:07:50 -0700

    Initial Commit

---
+++ Fitness Buddy/Fitness_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_BuddyApp.swift
+//  Fitness Buddy
+//
+//  Created by Alex Le on 2026-05-26.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fitness BuddyTests/Fitness_BuddyTests.swift
@@ -0,0 +1,19 @@
+//
+//  Fitness_BuddyTests.swift
+//  Fitness BuddyTests
+//
+//  Created by Alex Le on 2026-05-26.
+//
+
+import Testing
+@testable import Fitness_Buddy
+
+struct Fitness_BuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
